0d834ab6af3ce0d9557721da6bbef65396a0eb4e
ras0q <ras0q@users.noreply.github.com>
2025-03-10 00:37:03 +0900

    Add third-pary services information

--- src/views/SettingTab.ts
+++ src/views/SettingTab.ts
@@ -27,6 +27,57 @@ export class SettingTab extends PluginSettingTab {
     const { containerEl } = this;
     containerEl.empty();
 
+    const information = document.createDocumentFragment();
+    information.createSpan({
+      text:
+        "This plugin uses some third-party services and libraries. See the following links for more information:",
+    });
+    const informationList = information.createEl("ul", {}, (ul) => {
+      ul.createEl(
+        "li",
+        { text: "Reference metadata: Unpaywall API" },
+        (li) => {
+          li.createEl("ul", {}, (ul) => {
+            ul.createEl(
+              "li",
+              { text: "API Documentation (Data format): " },
+              (li) => {
+                li.createEl("a", {
+                  text: "unpaywall.org/data-format",
+                  href: "https://unpaywall.org/data-format",
+                });
+              },
+            );
+          });
+        },
+      );
+      ul.createEl(
+        "li",
+        { text: "Template engine: squirrelly" },
+        (li) => {
+          li.createEl("ul", {}, (ul) => {
+            ul.createEl("li", { text: "Documentation: " }, (li) => {
+              li.createEl("a", {
+                text: "squirrelly.js.org/docs",
+                href: "https://squirrelly.js.org/docs",
+              });
+            });
+            ul.createEl("li", { text: "GitHub Repository: " }, (li) => {
+              li.createEl("a", {
+                text: "squirrellyjs/squirrelly",
+                href: "https://github.com/squirrellyjs/squirrelly",
+              });
+            });
+          });
+        },
+      );
+    });
+    information.append(informationList);
+
+    new Setting(containerEl)
+      .setName("Third-party Services Information")
+      .setDesc(information);
+
     new Setting(containerEl)
       .setName("Email")
       .setDesc("Your email address for the Unpaywall API.")
@@ -43,7 +94,7 @@ export class SettingTab extends PluginSettingTab {
     new Setting(containerEl)
       .setName("Directory Template")
       .setDesc(
-        "The template for the directory structure of the reference notes. Use Mustache syntax.",
+        "The template for the directory structure of the reference notes",
       )
       .addText((text) =>
         text
@@ -58,7 +109,7 @@ export class SettingTab extends PluginSettingTab {
     new Setting(containerEl)
       .setName("Filename Template")
       .setDesc(
-        "The template for the filenames of the reference notes. Use Mustache syntax.\n" +
+        "The template for the filenames of the reference notes\n" +
           "Unknown characters in filenames will be replaced by underscores",
       )
       .addText((text) =>
@@ -73,10 +124,11 @@ export class SettingTab extends PluginSettingTab {
 
     new Setting(containerEl)
       .setName("Template Path")
-      .setDesc("The path to the Mustache template for the reference notes.")
+      .setDesc("The path to the template for the reference notes.")
       .addText((text) =>
         text
           .setPlaceholder("path/to/template.md")
+          .setValue(this.plugin.settings.templatePath)
           .onChange((value) => {
             this.plugin.settings.templatePath = value;
             this.plugin.saveSettings();
